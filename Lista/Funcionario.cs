896ea5f1d818d9c3d5a601ba92d755e752dd378c
Eneudes <eneudes.ap@gmail.com>
2019-02-13 16:51:34 -0300

    criado classe funcionario e programa pesquisa  o ID do funcionario se exite adciona uma porcetagem no salario

---
+++ Lista/Funcionario.cs
@@ -0,0 +1,20 @@
+﻿namespace Lista
+{
+    class Funcionario
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public double Salario { get; set; }
+
+        public Funcionario(int id, string nome, double salario)
+        {
+            Id = id;
+            Nome = nome;
+            Salario = salario;
+        }
+        public void IncrementarSalario(double porcentagem)
+        {
+            Salario += Salario * porcentagem / 100.0;
+        }
+    }
+}
---
+++ Lista/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace Lista
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Entar com quantidade de funcionario? ");
+            int qtd = int.Parse(Console.ReadLine());
+
+            List<Funcionario> funcionarios = new List<Funcionario>();
+
+            for(int i = 1; i <= qtd; i++)
+            {
+                Console.WriteLine("Funcionario #" + i);
+                Console.Write("Id: ");
+                int id = int.Parse(Console.ReadLine());
+                Console.Write("Nome: ");
+                string nome = Console.ReadLine();
+                Console.Write("Salario: ");
+                double salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                funcionarios.Add(new Funcionario(id, nome, salario));
+                Console.WriteLine();
+            }
+            Console.Write("Digite o Id do funcionario a ser alterado: ");
+            int idFunc = int.Parse(Console.ReadLine());
+
+            Funcionario pequisaId = funcionarios.Find(x => x.Id == idFunc);
+            if(pequisaId != null)
+            {
+                Console.Write("Valor da porcentagem: ");
+                double pct = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                pequisaId.IncrementarSalario(pct);
+            }
+            else
+            {
+                Console.WriteLine("Esse Id e invalido! ");
+            }
+
+            foreach(Funcionario obj in funcionarios)
+            {
+                
+                Console.WriteLine(obj.Id +","+ obj.Nome +","+ obj.Salario);
+            }
+        }
+    }
+}
